7a86c0f1fe12e6fe9f7daad9a1dd79d1c601962a
mayboolean <>
2024-12-09 17:45:46 -0500

    wave 1, app takes in message data and passes in sender, body and timestamp into ChatEntry. ChatEntry then fills in the element with info passed in from prop.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import messages from './data/messages.json';
+import TimeStamp from './components/TimeStamp';
 
 const App = () => {
+  const {sender, body, timeStamp} = messages[0];
+  // console.log(timeStamp);
+  // console.log(TimeStamp(timeStamp));
+  // console.log(sender);
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        <p>Hi</p>
+        <ChatEntry sender={sender} body={body} timeStamp={timeStamp} ></ChatEntry>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
