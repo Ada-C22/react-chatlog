ac367de2e39dd583ad3295a7873bc32894e58011
mayboolean <>
2024-12-09 21:54:59 -0500

    implements toggling heart icon

--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -1,8 +1,27 @@
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
+import { useState } from 'react';
 
 const ChatEntry = (props) => {
+  const [isLiked, setIsLiked] = useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const [count, setCount] = useState(0);
+
+  const toggleLikes = () => {
+    setIsLiked(!isLiked);
+    if (!isLiked) { // then it was previously unliked
+      const newCount = 1;
+      setCount(newCount); // then it was liked
+      console.log(newCount);
+    }
+    else {
+      const newCount = 0;
+      setCount(newCount);
+      console.log(newCount);
+    }
+  };
+  const heart = isLiked ? '❤️': '🤍';
   const localOrRemote = props.sender == props.local ? 'chat-entry local' : 'chat-entry remote';
   const meOrYou = props.sender == props.local ? 'me: ': '';
   return (
@@ -11,7 +30,7 @@ const ChatEntry = (props) => {
       <section className="entry-bubble">
         <p>{props.body}</p>
         <p className="entry-time"><TimeStamp time={props.timeStamp}> </TimeStamp></p>
-        <button className="like">🤍</button>
+        <button className="hearts" onClick={toggleLikes}>{heart}</button>
       </section>
     </div>
   );
