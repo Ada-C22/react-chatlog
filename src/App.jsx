871f3408b73c2d139bf6dcf2b2ac9f9617542f18
rad-hi7 <53797949+rad-hi7@users.noreply.github.com>
2024-12-10 00:41:05 -0500

    Loaded chat log by user using json data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import chatData from './data/messages.json';
 
 const App = () => {
+  const message = chatData.map((chat, index) => {
+    return (
+      <div key={index}>
+        <ChatEntry
+          sender={chat["sender"]}
+          body={chat["body"]}
+          timeStamp={chat["timeStamp"]}
+        ></ChatEntry>
+      </div>
+    );
+  });
   return (
     <div id="App">
       <header>
@@ -9,6 +22,7 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        {message}
       </main>
     </div>
   );
