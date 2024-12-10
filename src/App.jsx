0fab2675b7e23aff05b64e44c373c7868297e4a0
Priyanka <pri.ppk@gmail.com>
2024-12-09 16:48:42 -0800

    Add first implementation of wave 1.Additional refactors needed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,55 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+
 
 const App = () => {
+  // const chatData = [
+  //     {
+  //       sender: "Vladimir",
+  //       body: "why are you arguing with me",
+  //       timeStamp: "2018-05-29T22:49:06+00:00"
+  //     },
+  //     {
+  //       sender: "Estragon",
+  //       body: "Because you are wrong.",
+  //       timeStamp: "2018-05-29T22:49:33+00:00"
+  //     },
+  //     {
+  //       sender: "Vladimir",
+  //       body: "because I am what",
+  //       timeStamp: "2018-05-29T22:50:22+00:00"
+  //     },
+  //     {
+  //       sender: "Estragon",
+  //       body: "A robot.",
+  //       timeStamp: "2018-05-29T22:52:21+00:00"
+  //     },
+  //     {
+  //       sender: "Vladimir",
+  //       body: "how did you know",
+  //       timeStamp: "2018-05-29T22:52:58+00:00"
+  //     }];
+      const chatData = [
+        {
+          sender: "Joe Biden",
+          body: "Get out by 8am. I'll count the silverware",
+          timeStamp: "2018-05-18T22:12:03Z",
+          liked: false
+        }
+  ];
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={chatData[0].sender}
+          body={chatData[0].body}
+          timeStamp={chatData[0].timeStamp}
+          liked={chatData[0].liked}
+        />
+          {/*{Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -1,12 +1,22 @@
-import './ChatEntry.css';
+// update chatEntry and app to display:
+// 1. chat message bubble with the message text and relative timestamp,
+// 2. plus the sender's name above it.  
+// `ChatEntry` props: `sender`, `body`,  and `timeStamp`
+// do not need to include the `id` and `liked` fields for Wave 01
+// use an instance of the `TimeStamp` component
+// The `TimeStamp` component expects one `prop`: `time`
 
-const ChatEntry = () => {
+
+import './ChatEntry.css';
+import TimeStamp from './TimeStamp';
+
+const ChatEntry = ({sender, body, timeStamp, liked}) => {
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{body}</p>
+        <p className="entry-time"><TimeStamp time={timeStamp}></TimeStamp></p>
         <button className="like">🤍</button>
       </section>
     </div>
