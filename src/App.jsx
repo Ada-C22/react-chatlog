d69fe86b52c15066f16b4190e6d01142feae0f45
Priyanka <pri.ppk@gmail.com>
2024-12-12 15:34:54 -0800

    Add wave 2 and wave3 implementation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,49 @@
 import './App.css';
-import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
+import chatData from './data/messages.json';
+import { useState } from 'react';
 
+// Helper function to count total likes
+const calculateTotalLikes = (chatData) => {
+  let total = 0;
+  for (const like of chatData) {
+    total += like.liked;
+  }
+  return total;
+};
 
 const App = () => {
-  const chatData = [
-    {
-      sender: 'Vladimir',
-      body: 'why are you arguing with me',
-      timeStamp: '2018-05-29T22:49:06+00:00'
-    },
-    {
-      sender: 'Estragon',
-      body: 'Because you are wrong.',
-      timeStamp: '2018-05-29T22:49:33+00:00'
-    },
-    {
-      sender: 'Vladimir',
-      body: 'because I am what',
-      timeStamp: '2018-05-29T22:50:22+00:00'
-    },
-    {
-      sender: 'Estragon',
-      body: 'A robot.',
-      timeStamp: '2018-05-29T22:52:21+00:00'
-    },
-    {
-      sender: 'Vladimir',
-      body: 'how did you know',
-      timeStamp: '2018-05-29T22:52:58+00:00'
-    }];
+  const [likeData, setLikeData] = useState(chatData);
+
+  const handleLikeData = (id) => {
+    setLikeData(likeData => likeData.map(like => {
+      if (like.id === id) {
+        return { ...like, liked: !like.liked };
+      }
+      else {
+        return like;
+      }
+    }));
+  };
+
+  const totalLikes = calculateTotalLikes(likeData);
+
+  const formatLikes = (totalLikes) => {
+    if (totalLikes > 0) {
+      return `${totalLikes} ❤️s`;
+    } else {
+      return `${totalLikes} 🤍s`;
+    }
+  };
 
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h2>Chat between {chatData[0].sender} and {chatData[1].sender}</h2>
+        <h2>{formatLikes(totalLikes)}</h2>
       </header>
       <main>
-        <ChatEntry
-          sender={chatData[0].sender}
-          body={chatData[0].body}
-          timeStamp={chatData[0].timeStamp}
-          liked={chatData[0].liked}
-        />
-          {/*{Wave 02: Render ChatLog component */}
+        <ChatLog entries={likeData} handleLike={handleLikeData} />
       </main>
     </div>
   );
--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -1,21 +1,27 @@
 import './ChatEntry.css';
 import TimeStamp from './TimeStamp';
+import PropTypes from 'prop-types';
 
-const ChatEntry = ({sender, body, timeStamp, liked}) => {
+const ChatEntry = ({id, sender, body, timeStamp, liked, handleLike}) => {
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
         <p>{body}</p>
         <p className="entry-time"><TimeStamp time={timeStamp}></TimeStamp></p>
-        <button className="like">🤍</button>
+        <button className="like" onClick={() => handleLike(id)}>{liked ? '❤️' : '🤍'}</button>
       </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  // Fill with correct proptypes
+  id: PropTypes.number.isRequired,
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  handleLike: PropTypes.func.isRequired,
 };
 
 export default ChatEntry;
