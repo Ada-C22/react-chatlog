38e078cd4ea177afbd2268d7f2d261b02d98c763
Tatyana Venanzi <tatyanavenanzi@gmail.com>
2024-12-13 15:42:02 -0800

    moved entries array into app state, handled like toggle to update the liked method for specific messages, and calculated total  likes by adding a helper function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
+import React, { useState } from 'react';
 import './App.css';
 import ChatLog from './components/ChatLog';
 import messages from './data/messages.json';
 
 const App = () => {
+  const [chatEntries, setChatEntries] = useState(messages);
+
+  const toggleLike = (id) => {
+    setChatEntries((previousEntries) => 
+      previousEntries.map((entry) => 
+        entry.id == id ? {...entry, liked: !entry.liked } : entry
+      )
+    );
+  };
+
+  const totalLikes = chatEntries.filter((entry) => entry.liked).length;
   return (
     <div id="App">
       <header>
         <h1>Chat App</h1>
+        <p>{totalLikes} ❤️s</p>
       </header>
       <main>
-        <ChatLog entries={messages}/>
+        <ChatLog entries={chatEntries} onLikeToggle={toggleLike}/>
       </main>
     </div>
   );
