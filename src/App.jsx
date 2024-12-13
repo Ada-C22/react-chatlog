20820533f5b6390a6d09f65084ab630cf7d6acc0
Wei <qwbu22@gmail.com>
2024-12-12 20:38:20 -0800

    wave 1 & wave 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import React from 'react';
 import './App.css';
+import messages from './data/messages.json';
+import ChatLog from './components/ChatLog';
+
+
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat Between Vladimir and Estragon</h1>
+        <h2>❤️</h2>
       </header>
       <main>
+        <ChatLog entries={messages} />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -1,12 +1,14 @@
+import PropTypes from 'prop-types';
 import './ChatEntry.css';
+import TimeStamp from './TimeStamp';
 
-const ChatEntry = () => {
+const ChatEntry = (props) => {
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{props.body}</p>
+        <p className="entry-time"><TimeStamp time={props.timeStamp} /></p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -15,6 +17,13 @@ const ChatEntry = () => {
 
 ChatEntry.propTypes = {
   // Fill with correct proptypes
+  // id: PropyTypes.number.isRequired,
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+
+
+
 };
 
 export default ChatEntry;
