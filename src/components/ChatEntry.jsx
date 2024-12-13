f75835b48da7b74597b6afd5571a7f1f518f456d
Tatyana Venanzi <tatyanavenanzi@gmail.com>
2024-12-13 15:33:13 -0800

    added liked prop to show whether message is liked, added an event handler to toggle between red heart and white heart

--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -2,7 +2,10 @@ import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
-const ChatEntry = ({sender, body, timeStamp}) => {
+const ChatEntry = ({ id, sender, body, timeStamp, liked, onLikeToggle}) => {
+  const handleLikeClick = () => {
+    onLikeToggle(id);
+  };
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{sender}</h2>
@@ -11,17 +14,20 @@ const ChatEntry = ({sender, body, timeStamp}) => {
         <p className="entry-time">
           <TimeStamp time={timeStamp}/>
         </p>
-        <button className="like">🤍</button>
+        <button className="like" onClick={handleLikeClick}>{liked ? '❤️' : '🤍'}
+        </button>
       </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  // Fill with correct PropTypes
+  id: PropTypes.number.isRequired,
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  onLikeToggle: PropTypes.func.isRequired,
 };
 
 export default ChatEntry;
