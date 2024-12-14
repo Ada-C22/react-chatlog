36ed91b6e530b0b1868ce33c712d09e22c2bebc8
Weixi He <heweixiguge@gmai.com>
2024-12-14 10:39:37 -0500

    finish wave1-3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,45 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import data from './data/messages.json';
+import ChatLog from './components/ChatLog';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
+  const [messages, setMessages] = useState(data); 
+
+  const totalLikes = messages.filter((message) => message.liked).length;
+
+  const toggleLike = (id) => {
+    const updatedMessages = messages.map((message) =>
+      message.id === id ? { ...message, liked: !message.liked } : message
+    );
+    setMessages(updatedMessages);
+  };
+  const likeColor = totalLikes > 0 ? 'green' : 'red';
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1 className={likeColor}>{totalLikes} ❤️s</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatLog entries={messages} onLikeToggle={toggleLike} />
       </main>
     </div>
   );
+};
+
+App.propTypes = {
+  messages: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      sender: PropTypes.string.isRequired,
+      body: PropTypes.string.isRequired,
+      timeStamp: PropTypes.string.isRequired,
+      liked: PropTypes.bool.isRequired,
+    })
+  ),
 };
 
 export default App;
