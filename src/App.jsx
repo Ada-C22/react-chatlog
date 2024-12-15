b571d2493bf1286baa277f280001633ec37bf4a9
Lula <mlusan95@gmail.com>
2024-12-15 18:51:14 -0500

    project complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
 import './App.css';
+import ChatLog from './components/ChatLog';
+import { useState } from 'react';
+import messageData from './data/messages.json';
 
 const App = () => {
+  const [entryData, setEntryData] = useState(messageData);
+
+  const handleLikeEntry = (id) => {
+    setEntryData(entryData => entryData.map(entry =>{
+      if (entry.id === id) {
+        return {...entry, liked: !entry.liked};
+      } else {
+        return entry;
+      }
+    }));
+  };
+
+  const calculateTotalLikes = (entryData) => {
+    let total = 0;
+    for (const entry of entryData) {
+      if (entry.liked) {
+        total += 1;
+      }
+    };
+    return total;
+  };
+
+  const totalLikes = calculateTotalLikes(entryData);
+
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
+        <h2>Total Number of Likes: {totalLikes} ❤️s</h2>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatLog
+          entries={entryData}
+          onLikeEntry={handleLikeEntry}
+        />
       </main>
     </div>
   );
