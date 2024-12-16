43e9fac680e854e828d1ea4e35a297676a44f459
Brianna Root <brianna.k.root@gmail.com>
2024-12-16 18:02:41 +1300

    Updated Apps and ChatEntry to display a bubble with json data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import messages from './data/messages.json';
+
+
+// const samepleMessage = messages[0];
 
 const App = () => {
+  console.log(messages);
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry 
+          sender={samepleMessage.sender} 
+          body={samepleMessage.body} 
+          timeStamp={samepleMessage.timeStamp}
+        ></ChatEntry>
       </main>
     </div>
   );
