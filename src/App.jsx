69a40ce8784d50f10b9d9c86ba0a98508f776e56
somy <smycho28@gmail.com>
2024-12-15 22:23:45 -0800

    wave 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const chatData =
+  {
+    sender: 'Vladimir',
+    body: 'why are you arguing with me',
+    timeStamp: '2018-05-29T22:49:06+00:00'
+  };
+  const firstChat = chatData[0];
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chatlog</h1>
       </header>
       <main>
+        <ChatEntry
+          sender={firstChat.sender}
+          body={firstChat.body}
+          timeStamp={firstChat.timeStamp}
+        />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
